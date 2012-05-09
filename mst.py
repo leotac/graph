206a5322a85e164e8af351ff4f4f1ff10d712180
Leonardo Taccari <floor51@gmail.com>
2012-05-10 01:06:57 +0200

    eulerian tour

--- mst.py
+++ mst.py
@@ -1,3 +1,62 @@
+from gurobipy import *
+
+def euler(nodes, arcs):
+    n=len(nodes)
+
+    # Create adjacency list
+    adj = {}
+    for arc in arcs:
+         adj.setdefault(arc[0],[]).append(arc[1])
+         adj.setdefault(arc[1],[]).append(arc[0])
+
+    tours={} # Eulerian subtour starting in [node]
+
+    for node in nodes:
+            print "--------------------",node
+            tour=[node]
+            while len(adj[node])>0:
+                print node
+
+                i=adj[node][0]
+                adj[node].remove(i)
+                adj[i].remove(node)
+                node=i
+                tour.append(node)
+                
+            print tour
+            tours.setdefault(node,[])
+            if len(tour)>1:
+                tours[node]=tour[:-1]
+
+    print "Tours:",tours
+
+
+    # Depth-first visit of the Eulerian subtours to build one single Eulerian tour
+    visited=dict.fromkeys(nodes,False)
+    
+    tour=[]
+    euler=[]
+    stack=[nodes[0]]
+    print stack
+    while len(stack)>0:
+        node=stack.pop()
+        euler.append(node)
+        if visited[node]==False:
+            visited[node]=True
+            tour.append(node)
+            print node
+            stack.extend(tours[node])
+    print euler
+    print tour
+            
+                    
+                          
+                          
+            
+            
+            
+
+
 
 
 class unionfind:
